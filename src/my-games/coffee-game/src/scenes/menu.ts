ccb246eda0ff4fd2c7d095c2b178aac0c4f2118d
Ayushman <ayushman.ghosh_asp22@ashoka.edu.in>
2023-04-28 18:12:36 +0530

    Add Main menu scene

---
+++ src/my-games/coffee-game/src/scenes/menu.ts
@@ -0,0 +1,66 @@
+export class MenuScene extends Phaser.Scene{
+    
+    
+    constructor() {
+        
+        super({key : 'MenuScene'})
+    }
+
+    preload(): void {
+        
+        this.cameras.main.setBackgroundColor(0x75472d)
+
+        this.load.image('crossaint', 'images/croissant.png')
+       
+
+    }
+
+    create(): void {
+        const SCENE_CENTER_X = this.cameras.main.centerX - (this.cameras.main.centerX/16)
+        const SCENE_CENTER_Y = this.cameras.main.centerY +  (this.cameras.main.centerY/2)
+        
+        
+        let crossaint = this.add.sprite(0,0, 'crossaint')
+        
+        if(crossaint){
+        
+
+            crossaint.setPosition( 
+                SCENE_CENTER_X,
+                SCENE_CENTER_Y,
+                
+                )
+            crossaint.setScale(2)
+        }
+
+        this.tweens.add({
+            targets: crossaint,
+            angle: 360,
+            duration: 5600,
+            loop: -1
+        })
+        let text = this.add.text(
+            0 , 
+            0 ,
+             'Press X to start'
+             )
+
+       
+        text.setPosition( 
+            (SCENE_CENTER_X - (SCENE_CENTER_X/5)),
+            (SCENE_CENTER_Y - (SCENE_CENTER_Y/2))
+        )
+
+
+
+        // this.anims.create(
+        //     {
+        //         key: 'rot_crossaint',
+        //         frameRate: 7,
+        //         frames: this.anims.generateFrameNumbers()
+        //     }
+        // )
+
+    }
+
+}
