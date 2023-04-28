d41518e09fa61e74bf5a18ba9d4f692b1f997c5b
Ayushman <ayushman.ghosh_asp22@ashoka.edu.in>
2023-04-28 23:28:59 +0530

    Adjusted camera scaling

--- src/my-games/coffee-game/src/scenes/main-scene.ts
+++ src/my-games/coffee-game/src/scenes/main-scene.ts
@@ -8,18 +8,28 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload(): void {
-    this.load.image("tiles", "maps/tilesheet_basic.png");
-    this.load.tilemapTiledJSON("map", "maps/TestMap.json");
+    this.load.image("doors", "maps/TopDownHouse_DoorsAndWindows.png");
+    this.load.image("floor", "maps/TopDownHouse_FloorsAndWalls.png");
+    this.load.tilemapTiledJSON("map", "maps/Cafe_main_floor.json");
   }
 
   create(): void {
     const map = this.make.tilemap({ key: "map" });
+    const spawnPoint = map.findObject("spawnPoint", obj => obj.name === "spawnPointPlayer")
+    this.cameras.main.zoomTo(3.5,0)
+    this.cameras.main.centerOn((spawnPoint.x + 30), (spawnPoint.y - 30));
 
+    
     // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
     // Phaser's cache (i.e. the name you used in preload)
-    const tileset = map.addTilesetImage("TestTileset", "tiles");
-  
+    const floor = map.addTilesetImage("Cafe_floor_walls", "floor");
+    const door = map.addTilesetImage("Cafe_doors_windows", "doors")
+   
+    
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const worldLayer = map.createLayer("Tile Layer 1", tileset, 0, 0);
+    const floorLayer = map.createLayer("Floor", floor, 0, 0);
+    const doorLayer = map.createLayer("door", door, 0, 0);
+    
+    
   }
 }
